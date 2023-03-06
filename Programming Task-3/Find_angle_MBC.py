4579da53c50fd2b9e330ce2b9cafbfb1d48af7a5
JAI INDRA REDDY <81897404+indra2304-alpha@users.noreply.github.com>
2023-03-06 23:38:38 +0530

    Add files via upload

---
+++ Programming Task-3/Find_angle_MBC.py
@@ -0,0 +1,6 @@
+import math
+
+a = int(input())
+b = int(input())
+
+print(f'{math.degrees(math.atan(a / b)):1.0f}\xb0')
---
+++ Programming Task-3/polar_coordinates.py
@@ -0,0 +1,7 @@
+import cmath
+
+a,b = cmath.polar(complex(input()))
+
+print(a)
+
+print(b)
